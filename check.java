ae7716c40a0dcdc463178449961165015518f03a
guribaby <122307456+guribaby@users.noreply.github.com>
2023-05-03 20:38:55 -0600

    Create check.java

---
+++ check.java
@@ -0,0 +1,44 @@
+public class verificador {
+    int max;
+    int top;
+    public char[] stack;
+
+    public verificador(int stackSize) {
+        this.stack = new char[stackSize];
+        this.max = this.stack.length;
+        this.top = -1;
+    }
+
+    public void push(char value) {
+        if (top == max - 1) {
+            System.out.println("Capacidad llena");
+            return;
+        }
+        top++;
+        stack[top] = value;
+    }
+
+    public char pop() {
+        if (top == -1) {
+            System.out.println("Espacio libre");
+            return '\0';
+        }
+        char value = stack[top];
+        top--;
+        return value;
+    }
+
+    public String invertirCadena(String cadEnt) {
+        verificador stack = new verificador(cadEnt.length());
+
+        for (int i = 0; i < cadEnt.length(); i++) {
+            stack.push(cadEnt.charAt(i));
+        }
+
+        String cadSal = "";
+        while (stack.top != -1) {
+          cadSal = cadSal + stack.pop();
+        }
+        return cadSal;
+    }
+}   
